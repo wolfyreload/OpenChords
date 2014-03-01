d588a9240c2f8ac9d3cea2e9ced70c1a0a86d9dd
WolfyReload <open.chords.app@gmail.com>
2014-03-01 09:03:04 +0200

    -updated the build file to add the web site to the build

--- build.bat
+++ build.bat
@@ -3,6 +3,7 @@ mkdir build
 mkdir build\OpenChords
 mkdir build\OpenChords\App
 mkdir build\OpenChords\App\Bin
+mkdir build\OpenChords\Web
 
 cd OpenChords\
 call build.bat
@@ -22,6 +23,23 @@ xcopy  /Y .\OpenChords\manual\manual.pdf ".\build\OpenChords\App\Bin"
 xcopy  /Y .\OpenChords\changelog\changelog.txt ".\build\OpenChords\App\Bin" 
 
 xcopy /Y /E .\OpenChordsData\* ".\build\OpenChords\App\Data\"
+
+xcopy /Y /E .\OpenChords\OpenChords.Web\* .\build\OpenChords\Web\
+RMDIR /Q /S .\build\OpenChords\Web\App_Code
+RMDIR /Q /S .\build\OpenChords\Web\App_Data
+RMDIR /Q /S .\build\OpenChords\Web\obj
+RMDIR /Q /S .\build\OpenChords\Web\Properties
+RMDIR /Q /S .\build\OpenChords\Web\Scripts
+RMDIR /Q /S .\build\OpenChords\Web\Bin\Scripts
+RMDIR /Q /S .\build\OpenChords\Web\Bin\App_Themes
+DEL /F .\build\OpenChords\Web\Web.Debug.config
+DEL /F .\build\OpenChords\Web\Web.Release.config
+DEL /F /S /Q .\build\OpenChords\Web\*.cs
+DEL /F /S /Q .\build\OpenChords\Web\*.csproj
+DEL /F /S /Q .\build\OpenChords\Web\*.user
+DEL /F /S /Q .\build\OpenChords\Web\Bin\*.pdb
+
+
 
 echo cd app\bin >> .\build\OpenChords\StartOpenChords.bat
 echo start OpenChords.exe >> .\build\OpenChords\StartOpenChords.bat
