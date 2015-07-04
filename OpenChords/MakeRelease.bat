3215ccba1c671b91f0949076b81a3c69f5357d70
wolfyreload <open.chords.app@gmail.com>
2015-07-04 12:31:40 +0200

    Improve MakeRelease.bat script to do a little more cleaning and work with the new help documentation

--- OpenChords/MakeRelease.bat
+++ OpenChords/MakeRelease.bat
@@ -14,7 +14,7 @@ xcopy  /Y /E .\OpenChords.CrossPlatform.Gtk2\bin\Release\*   .\Staging\Gtk2App\
 
 :: copy the manual
 xcopy  /Y /E .\Manual\Help.html .\Staging\Manual\
-xcopy  /Y /E .\Manual\HelpImages\*.png .\Staging\Manual\HelpImages
+xcopy  /Y /E .\Manual\HelpImages\* .\Staging\Manual\HelpImages
 
 :: make settings file for legacy version
 copy /Y .\portable.xml .\Staging\LegacyApp\settings.xml
@@ -23,6 +23,14 @@ copy /Y .\portable.xml .\Staging\LegacyApp\settings.xml
 xcopy /Y /E .\Staging\Manual\* .\Staging\LegacyApp\
 xcopy /Y /E .\Staging\Manual\* .\Staging\WpfApp\
 xcopy /Y /E .\Staging\Manual\* .\Staging\Gtk2App\ 
+
+::cleanup files in staging
+pushd Staging
+del /s *.pdb
+del /s *Eto.xml
+del /s *log4net.xml
+del /s *.vshost.exe*
+popd
 
 echo cd app >> .\staging\StartOpenChords.bat
 echo start OpenChords.exe >> .\staging\StartOpenChords.bat
