0f06bfe5e513f5288fb5a2b06a52d94bd6baa2c0
wolfyreload <open.chords.app@gmail.com>
2015-08-15 17:33:46 +0200

    Change command line build.bat to work with MsBuild v14

--- OpenChords/build.bat
+++ OpenChords/build.bat
@@ -1,2 +1,2 @@
 rmdir /Q /S OpenChordsLoader\Bin\Release
-"C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" OpenChords.sln /t:Clean,Build /p:Configuration=Release >build.log
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" OpenChords.sln /t:Clean,Build /p:Configuration=Release >build.log
