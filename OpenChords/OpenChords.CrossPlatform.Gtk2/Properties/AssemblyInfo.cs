e604bfe12178b49a785b40d382fe037ebb4a8de5
wolfyreload <open.chords.app@gmail.com>
2015-08-17 16:07:21 +0200

    Fixed: Renamed executable file names and added OpenChords icon to all executables

--- OpenChords/OpenChords.CrossPlatform.Gtk2/Properties/AssemblyInfo.cs
+++ OpenChords/OpenChords.CrossPlatform.Gtk2/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("OpenChords.CrossPlatform.Gtk2")]
+[assembly: AssemblyTitle("OpenChords.Linux.Gtk2")]
