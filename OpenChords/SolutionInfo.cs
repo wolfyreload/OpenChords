a2e385ddba6ce79b39405dbac476262b9f266046
wolfyreload <open.chords.app@gmail.com>
2015-08-22 13:10:28 +0200

    Bump Version number to 2.0.0

--- OpenChords/SolutionInfo.cs
+++ OpenChords/SolutionInfo.cs
@@ -19,5 +19,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 
