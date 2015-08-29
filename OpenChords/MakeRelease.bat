db9fe01e77386b25d86247439caa7ac250f01230
wolfyreload <open.chords.app@gmail.com>
2015-08-29 11:50:08 +0200

    Fixed: Adjusted release path

--- OpenChords/MakeRelease.bat
+++ OpenChords/MakeRelease.bat
@@ -6,20 +6,20 @@ call build.bat
 call Manual\MakeHelpDocument.bat
 
 :: copy all needed files to run the application
-xcopy  /Y /E .\OpenChords.CrossPlatform.Wpf\bin\Release\*    .\Staging\WindowsApp\App\
-xcopy  /Y /E .\OpenChords.CrossPlatform.Gtk2\bin\Release\*   .\Staging\LinuxApp\App\ 
+xcopy  /Y /E .\OpenChords.CrossPlatform.Wpf\bin\Release\*    .\Staging\WindowsApp\OpenChords\App\
+xcopy  /Y /E .\OpenChords.CrossPlatform.Gtk2\bin\Release\*   .\Staging\LinuxApp\OpenChords\App\ 
 
 :: copy the manual
 xcopy  /Y /E .\Manual\Help.html .\Staging\Manual\
 xcopy  /Y /E .\Manual\HelpImages\* .\Staging\Manual\HelpImages
 
 ::Put manual into each folder
-xcopy /Y /E .\Staging\Manual\* .\Staging\WindowsApp\App\
-xcopy /Y /E .\Staging\Manual\* .\Staging\LinuxApp\App\ 
+xcopy /Y /E .\Staging\Manual\* .\Staging\WindowsApp\OpenChords\App\
+xcopy /Y /E .\Staging\Manual\* .\Staging\LinuxApp\OpenChords\App\ 
 
 ::copy the settings files
-xcopy /Y .\settings.xml .\Staging\WindowsApp\App
-xcopy /Y .\settings.xml .\Staging\LinuxApp\App 
+xcopy /Y .\settings.xml .\Staging\WindowsApp\OpenChords\App
+xcopy /Y .\settings.xml .\Staging\LinuxApp\OpenChords\App 
 
 ::Make zip files for portable version
 7za.exe a -tzip .\Release\OpenChords.Windows.zip .\Staging\WindowsApp\* 
