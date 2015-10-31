d01748f1e4215dc01b6d70ff5e0085104e48ec2d
wolfyreload <open.chords.app@gmail.com>
2015-10-31 07:59:32 +0200

    Improvement: Added help text for when the http server is enabled to help users use this feature

--- OpenChords/OpenChords.CrossPlatform/Preferences/GeneralSettingsPreferences.cs
+++ OpenChords/OpenChords.CrossPlatform/Preferences/GeneralSettingsPreferences.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace OpenChords.CrossPlatform.Preferences
@@ -19,6 +20,7 @@ namespace OpenChords.CrossPlatform.Preferences
         private Button cmdOpenSongSongsAndSetsPath = new Button() { Text = "", Style = "OpenFolder", Image = Graphics.ImageExplore };
         private CheckBox chkHttpServerEnabled = new CheckBox();
         private TextBox txtHttpServerPort = new TextBox() { Width = 50 };
+        private TextArea txtInterfaceHelp = new TextArea() { Height = 100 };
         private bool OptionsChanged;
 
         public GeneralSettingsPreferences(Entities.FileAndFolderSettings fileAndFolderSettings)
@@ -35,6 +37,7 @@ namespace OpenChords.CrossPlatform.Preferences
                     new TableRow(new Label() { Text = "OpenSong Data Folder" }, txtOpenSongDataPath, cmdOpenSongSongsAndSetsPath, new Label() ),
                     new TableRow(new Label() { Text = "Http Server Enabled" }, chkHttpServerEnabled ),
                     new TableRow(new Label() { Text = "Http Server Port" }, txtHttpServerPort ),
+                    new TableRow(new Label() { Text = "Http Examples" }, txtInterfaceHelp),
                     null
                 }
             };
@@ -45,6 +48,7 @@ namespace OpenChords.CrossPlatform.Preferences
             txtOpenSongDataPath.Text = fileAndFolderSettings.OpenSongSetsAndSongs;
             chkHttpServerEnabled.Checked = fileAndFolderSettings.HttpServerEnabled;
             txtHttpServerPort.Text = fileAndFolderSettings.HttpServerPort.ToString();
+            setHelpOnInterfaces(chkHttpServerEnabled.Checked ?? false);
 
             //events
             chkPortableMode.CheckedChanged += chkPortableMode_CheckedChanged;
@@ -52,12 +56,58 @@ namespace OpenChords.CrossPlatform.Preferences
             txtOpenSongDataPath.TextChanged += textChanged;
             txtOpenSongExecutablePath.TextChanged += textChanged;
             chkHttpServerEnabled.CheckedChanged += checkedChanged;
+            chkHttpServerEnabled.CheckedChanged += ChkHttpServerEnabled_CheckedChanged;
             txtHttpServerPort.TextChanged += textChanged;
+            txtHttpServerPort.TextChanged += TxtHttpServerPort_TextChanged;
             cmdApplicationDataFolder.Click += CmdApplicationDataFolder_Click;
             cmdOpenSongExecutablePath.Click += CmdOpenSongExecutablePath_Click;
             cmdOpenSongSongsAndSetsPath.Click += CmdOpenSongSongsAndSetsPath_Click;
         }
 
+        private void TxtHttpServerPort_TextChanged(object sender, EventArgs e)
+        {
+            setHelpOnInterfaces(chkHttpServerEnabled.Checked ?? true);
+        }
+
+        private void ChkHttpServerEnabled_CheckedChanged(object sender, EventArgs e)
+        {
+            setHelpOnInterfaces(chkHttpServerEnabled.Checked ?? true);
+        }
+
+        private void setHelpOnInterfaces(bool show)
+        {
+            if (show)
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.AppendLine("You can access OpenChords externally using:");
+                // Get host name
+                String strHostName = Dns.GetHostName();
+
+                // Find host by name
+                IPHostEntry iphostentry = Dns.GetHostEntry(strHostName);
+
+                // Enumerate IP addresses
+                foreach (IPAddress ipaddress in iphostentry.AddressList)
+                {
+                    if (!ipaddress.ToString().Contains(":")) //only show ip version 4 addresses
+                        sb.AppendLine(string.Format("http://{0}:{1}/song", ipaddress, txtHttpServerPort.Text));
+                }
+                if (OptionsChanged)
+                    sb.AppendLine("after restarting OpenChords");
+
+
+                txtInterfaceHelp.ReadOnly = false;
+                txtInterfaceHelp.Text = sb.ToString().Trim();
+                txtInterfaceHelp.ReadOnly = true;
+            }
+            else
+            {
+                txtInterfaceHelp.ReadOnly = false;
+                txtInterfaceHelp.Text = "You cannot access OpenChords externally since the http server is disabled";
+                txtInterfaceHelp.ReadOnly = true;
+            }
+        }
+        
         private void CmdApplicationDataFolder_Click(object sender, EventArgs e)
         {
             var path = getFolderPath();
