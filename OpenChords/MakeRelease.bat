2220f1084bd060819837ed6136b4d6cffc94084c
wolfyreload <open.chords.app@gmail.com>
2015-10-31 08:57:17 +0200

    Add help documentation build to part of application build.

--- OpenChords/MakeRelease.bat
+++ OpenChords/MakeRelease.bat
@@ -3,18 +3,9 @@ rmdir Release /Q /S
 
 call build.bat
 
-call Manual\MakeHelpDocument.bat
-
 :: copy all needed files to run the application
 xcopy  /Y /E .\OpenChords.CrossPlatform.Wpf\bin\Release\*    .\Staging\WindowsApp\OpenChords\App\
 xcopy  /Y /E .\OpenChords.CrossPlatform.Gtk2\bin\Release\*   .\Staging\LinuxApp\OpenChords\App\ 
-
-:: copy the manual
-xcopy  /Y .\Manual\Help.html .\Staging\Manual\
-
-::Put manual into each folder
-xcopy /Y /E .\Staging\Manual\* .\Staging\WindowsApp\OpenChords\App\
-xcopy /Y /E .\Staging\Manual\* .\Staging\LinuxApp\OpenChords\App\ 
 
 ::copy the settings files
 xcopy /Y .\settings.xml .\Staging\WindowsApp\OpenChords\App
