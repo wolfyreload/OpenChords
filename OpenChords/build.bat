1c3990c0127b0cf36638173bd0c0e59c16616958
wolfyreload <open.chords.app@gmail.com>
2015-11-29 14:06:15 +0200

    Improvement: Don't write builds to log file rather just display build in console

--- OpenChords/build.bat
+++ OpenChords/build.bat
@@ -3,7 +3,7 @@ rmdir /Q /S OpenChords.CrossPlatform.Wpf\Bin\Release
 rmdir /Q /S OpenChords.CrossPlatform.Gtk2\Bin\Release
 
 ::build application
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" OpenChords.sln /t:Clean,Build /p:Configuration=Release >build.log
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" OpenChords.sln /t:Clean,Build /p:Configuration=Release
 
 ::cleanup pdb and xml files
 del /S OpenChords.CrossPlatform.Gtk2\Bin\Release\*.pdb
