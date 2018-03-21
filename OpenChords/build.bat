1eb7bc1d09c9671d34cdfd72e72b106243a9e5cb
Michael van Antwerpen <michael.antwerpen@ewx.co.za>
2018-03-21 17:27:00 +0200

    Upgrade build tools to visual studio 2017
    * Remove old nuget package references
    * Change default start project to OpenChords.CrossPlatform.Executable

--- OpenChords/build.bat
+++ OpenChords/build.bat
@@ -4,7 +4,7 @@ rmdir /Q /S OpenChords.CrossPlatform.Executable\Bin\Release
 
 ::build application
 nuget restore OpenChords.sln
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" OpenChords.sln /t:Clean,Build /p:Configuration=Release /verbosity:minimal
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" OpenChords.sln /t:Clean,Build /p:Configuration=Release /verbosity:minimal
 
 ::cleanup pdb and xml files
 del /S OpenChords.CrossPlatform.Executable\Bin\Release\*.pdb
