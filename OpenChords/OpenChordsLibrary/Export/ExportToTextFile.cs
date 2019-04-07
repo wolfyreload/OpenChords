9ab6fc6ad4691e0c8d7dcf80cdde2f7888be33a4
Michael van Antwerpen <michael.antwerpen@ewx.co.za>
2019-04-07 06:44:09 +0200

    Improvement: Change export to list functionality to use xlsx rather than csv

--- OpenChords/OpenChordsLibrary/Export/ExportToTextFile.cs
+++ OpenChords/OpenChordsLibrary/Export/ExportToTextFile.cs
@@ -1,4 +1,5 @@
-﻿using OpenChords.Entities;
+﻿using OfficeOpenXml;
+using OpenChords.Entities;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -13,30 +14,61 @@ namespace OpenChords.Export
         public static string exportSetSongList(Set currentSet)
         {
             List<Song> listOfSongs = currentSet.songList;
-            string filename = $"{Settings.GlobalApplicationSettings.PrintFolder}{currentSet.setName}_{DateTime.Now:yyyy-MM-dd_HHmmss}.csv";
+            string filename = $"{Settings.GlobalApplicationSettings.PrintFolder}{currentSet.setName}_{DateTime.Now:yyyy-MM-dd_HHmmss}.xlsx";
             exportListToFile(listOfSongs, filename, currentSet.setName);
             return filename;
         }
 
         public static string exportSongList(List<Song> listOfSongs)
         {
-            string filename = $"{Settings.GlobalApplicationSettings.PrintFolder}Song_List_{DateTime.Now:yyyy-MM-dd_HHmmss}.csv";
+            string filename = $"{Settings.GlobalApplicationSettings.PrintFolder}Song_List_{DateTime.Now:yyyy-MM-dd_HHmmss}.xlsx";
             exportListToFile(listOfSongs, filename, "All Songs");
             return filename;
         }
 
         private static void exportListToFile(List<Song> listOfSongs, string nameOfFile, string setName = null)
         {
-            StringBuilder sb = new StringBuilder();
-            if (!string.IsNullOrWhiteSpace(setName))
-                sb.Append(setName + "\r\n\r\n");
-            sb.Append("Song Title\tAlternative Title\tReference\tKey & Capo\tOrder\tSub Folder\r\n");
-            foreach (Song song in listOfSongs)
+            using (ExcelPackage excelPackage = new ExcelPackage())
             {
-                sb.Append($"{song.title}\t{song.aka}\t{song.hymn_number}\t{song.getKeyAndCapo()}\t{song.presentation}\t{song.SongSubFolder}\r\n");
+                ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets.Add("SongList");
+
+                // Name
+                if (!string.IsNullOrWhiteSpace(setName))
+                {
+                    worksheet.Cells["A1"].Value = setName;
+                    worksheet.Row(1).Style.Font.Size = 14;
+                    worksheet.Row(1).Style.Font.Bold = true;
+                }
+
+                // Headings
+                worksheet.Cells["A3"].Value = "Song Title";
+                worksheet.Cells["B3"].Value = "Alternative Title";
+                worksheet.Cells["C3"].Value = "Reference";
+                worksheet.Cells["D3"].Value = "Key & Capo";
+                worksheet.Cells["E3"].Value = "Order";
+                worksheet.Cells["F3"].Value = "Sub Folder";
+                worksheet.Cells["G3"].Value = "CCLI";
+                worksheet.Row(3).Style.Font.Bold = true;
+
+                // Song content
+                int startRow = 4;
+                foreach (Song song in listOfSongs)
+                {
+                    worksheet.Cells[startRow, 1].Value = song.title;
+                    worksheet.Cells[startRow, 2].Value = song.aka;
+                    worksheet.Cells[startRow, 3].Value = song.hymn_number;
+                    worksheet.Cells[startRow, 4].Value = song.getKeyAndCapo();
+                    worksheet.Cells[startRow, 5].Value = song.presentation;
+                    worksheet.Cells[startRow, 6].Value = song.SongSubFolder;
+                    worksheet.Cells[startRow, 7].Value = song.ccli;
+
+                    startRow++;
+                }
+
+                worksheet.Cells[worksheet.Dimension.Address].AutoFitColumns();
+
+                excelPackage.SaveAs(new FileInfo(nameOfFile));
             }
-            string setText = sb.ToString();
-            File.WriteAllText(nameOfFile, setText);
         }
     }
 }
